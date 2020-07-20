cc07ddba011cc7072830148bf12ec86fcf5ba233
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-07-20 22:37:50 +0800

    modify setup

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from setuptools import setup
 
-
+from setuptools import setup, find_packages
+from os import path, environ
+ 
+from io import open
+ 
+here = path.abspath(path.dirname(__file__))
+ 
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='cryptoString',
-    version='0.0.2',
+    version='0.0.3',
     author='andy6804tw',
     author_email='andy6804tw@yahoo.com.tw',
     url='https://github.com/1010code/cryptoString',
